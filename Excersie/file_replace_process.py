0fbf9e546f7ee258729a292fdf4976a5761e4baa
git-smita <smita.pantula@adtran.com>
2018-08-30 08:23:13 +0530

    Delete file_replace_process.py

--- Excersie/file_replace_process.py
+++
@@ -1,8 +0,0 @@
-mountList = open("mount.txt", "r").readlines()
-print mountList
-newmountList = open("newmount.txt","r").readlines()
-outputList = [item for item in mountList if "filer" not in item.lower()]
-outputList.extend(newmountList)
-f = open("mount.txt","w")
-f.write(''.join(outputList))
-f.close()
