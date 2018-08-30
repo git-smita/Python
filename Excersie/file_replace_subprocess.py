7a1a2a1f93d425acf9ed68657dfc09b6d58fbfe9
git-smita <smita.pantula@adtran.com>
2018-08-30 08:23:41 +0530

    Delete file_replace_subprocess.py

--- Excersie/file_replace_subprocess.py
+++
@@ -1,6 +0,0 @@
-#import subprocess
-#p=subprocess.Popen('ls')
-#p.communicate()
-#subprocess.Popen('grep -irl meena * | xargs sed -i s/meena/meena/g')
-import os
-os.system('grep -irl meena * | xargs sed -i s/meena/meena/g')
