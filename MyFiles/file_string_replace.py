62eaeed5ed533cdcc6a32b260c9b9dafbf905b80
git-smita <smita.pantula@adtran.com>
2018-08-30 10:02:00 +0530

    Update file_string_replace.py
    other user

--- MyFiles/file_string_replace.py
+++ MyFiles/file_string_replace.py
@@ -3,7 +3,7 @@ fd = open('make.mk', 'r')
 tempstr = fd.read()
 fd.close()
 #to replace the file content don't open in append mode, write
-#  mode start with the first char location
+#  mode start with the first character location
 fd = open('make.mk', 'w')
 tempstr = tempstr.replace('long', 'long123')
 fd.write(tempstr)
